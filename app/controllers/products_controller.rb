6a19659d2a4ba964d3672092e35907d57fcb33b7
Sabrina Jones <sabrina.jones87@gmail.com>
2018-01-22 04:16:24 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-def index
+  def index
   @products = Product.all
 end
 
@@ -9,7 +9,6 @@ end
 
 def create
   @product = Product.create(product_params)
-  @product.save
   redirect_to products_path
 end
 
@@ -26,7 +25,12 @@ end
 private
 
 def product_params
-  params.require(:product).permit(:name, :price, :description, :inventory)
+  params.require(:product).permit(
+    :name,
+    :description,
+    :inventory,
+    :price
+  )
 end
 
 end
